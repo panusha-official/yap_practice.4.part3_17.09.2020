eaab59ba734cc7dc5039b56c2d4ac3b6341cb7ef
PaNuSHa <70700507+panusha-official@users.noreply.github.com>
2020-09-17 16:39:37 +0300

    Add files via upload

---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_4.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+class Program 
+{ 
+  static void Main(string[] args) 
+  { 
+    Hashtable lookup = new Hashtable();
+    
+    lookup["0"] = "Zero"; 
+    lookup["1"] = "One"; 
+    lookup["2"] = "Two"; 
+    lookup["3"] = "Three"; 
+    lookup["4"] = "Four"; 
+    lookup["5"] = "Five"; 
+    lookup["6"] = "Six"; 
+    lookup["7"] = "Seven"; 
+    lookup["8"] = "Eight"; 
+    lookup["9"] = "Nine"; 
+    
+    string ourNumber = "888-555-1212"; 
+    
+    foreach (char с in ourNumber) 
+    { 
+      string digit = с.ToString(); 
+      
+      if(lookup.ContainsKey(digit)) 
+      { 
+        Console.WriteLine(lookup[digit]); 
+      } 
+    } 
+  } 
+}
